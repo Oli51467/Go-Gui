564778ffcf2dbdb323c5744a7d2dac223de2ec74
Oli51467 <1832297226@qq.com>
2022-12-06 18:37:51 +0800

    fix：网络请求headers修改

--- apis.py
+++ apis.py
@@ -6,21 +6,21 @@ import config
 
 
 def init_set(data):
-    resp = requests.post(url=config.configs['production'].REQUEST_IP + "set", json=data)
+    resp = requests.post(url=config.configs['production'].REQUEST_IP + "set", json=data, headers={"Connection": "close"})
     if str(resp).find('502') != -1:
         return False
     return json.loads(resp.text)['code']
 
 
 def go(data):
-    resp = requests.post(url=config.configs['production'].REQUEST_IP + "go", json=data)
+    resp = requests.post(url=config.configs['production'].REQUEST_IP + "go", json=data, headers={"Connection": "close"})
     resp_data = json.loads(resp.text)['data']
     print(resp_data)
     return resp_data['pv'][0]
 
 
 def tip(data):
-    resp = requests.post(url=config.configs['production'].REQUEST_IP + "WholeGameTip", json=data)
+    resp = requests.post(url=config.configs['production'].REQUEST_IP + "WholeGameTip", json=data, headers={"Connection": "close"})
     resp_data = json.loads(resp.text)['data']
     print("resp data:", resp_data)
     return resp_data['move']
